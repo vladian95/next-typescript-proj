7716ca51c79dbb8dbd2db5f6a2b49b3537e677eb
vladian95 <kvankvan1995@gmail.com>
2023-05-30 11:22:31 +0300

    burger

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,17 +1,45 @@
 import { Link } from 'react-scroll';
 import Logo from '../Logo/Logo';
 import styles from '../../styles/header.module.scss';
+import { useMediaQuery } from '@/hooks';
+import { useState } from 'react';
 
 const Header = () => {
+  const isMobile = useMediaQuery(640);
+  const [menuOpen, setMenuOpen] = useState(false);
   const spy = true;
   const smooth = true;
   const offset = 140;
   const duration = 500;
 
+  const handleToggleMenu = () => {
+    (document.querySelector('body') as HTMLBodyElement).classList.toggle(
+      'overflow-hidden'
+    );
+    setMenuOpen(!menuOpen);
+  };
+
+  const closeMenu = () => {
+    (document.querySelector('body') as HTMLBodyElement).classList.remove(
+      'overflow-hidden'
+    );
+    setMenuOpen(false);
+  };
+
   return (
     <header className={styles.header}>
       <div className={`container ${styles.header__container}`}>
         <Logo />
+        {isMobile && (
+          <button
+            className={`${styles.burger_menu} ${menuOpen ? styles.open : ''}`}
+            onClick={handleToggleMenu}
+          >
+            <span />
+            <span />
+            <span />
+          </button>
+        )}
         <nav className={styles.header__nav}>
           <ul className={styles.header__nav__list}>
             <li className={styles.header__nav__list__item}>
